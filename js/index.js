b7209972499b06fd60795d9b8d4c7ff5603f8e93
ottebrut <ott.andrey.ao@gmail.com>
2020-11-07 18:58:22 +0300

    2.2

--- js/index.js
+++ js/index.js
@@ -3,3 +3,27 @@ document.querySelectorAll("tr").forEach(tr =>
     Array.from(tr.children).forEach((td, index) =>
         td.setAttribute("label", ths[index]))
 )
+
+const radioButtons = document.querySelectorAll(".radio-types__input");
+const reportTitle = document.querySelector(".report__title")
+const reportTable = document.querySelector(".report-table");
+const sorryMessage = document.querySelector(".report__sorry-message");
+
+generate();
+
+function generate() {
+    let checkedRadio;
+    radioButtons.forEach(radio => {
+        if (radio.checked) {
+            checkedRadio = radio.id.substr(6); // radio-...
+        }
+    });
+    reportTitle.innerText = checkedRadio[0].toUpperCase() + checkedRadio.slice(1) + " report";
+    if (checkedRadio === "calibration") {
+        reportTable.style.display = "table";
+        sorryMessage.style.display = "none";
+    } else {
+        reportTable.style.display = "none";
+        sorryMessage.style.display = "block";
+    }
+}
